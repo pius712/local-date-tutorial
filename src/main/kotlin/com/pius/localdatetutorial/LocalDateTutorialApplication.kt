dc953fab122eaf2d8aa59a0e332a366f5c48f8de
pius712 <aksfbsgnlfls@gmail.com>
2023-12-10 14:46:52 +0900

    first commit

---
+++ src/main/kotlin/com/pius/localdatetutorial/LocalDateTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.pius.localdatetutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LocalDateTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<LocalDateTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/pius/localdatetutorial/LocalDateTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pius.localdatetutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LocalDateTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
